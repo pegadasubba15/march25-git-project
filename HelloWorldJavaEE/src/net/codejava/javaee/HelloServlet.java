bb310b013ebd0110dfeafcc2860e37ee776a1d28
Kasikumar <kdabbugottu@apple.com>
2020-03-27 19:58:10 +0530

    jsut added :

--- HelloWorldJavaEE/src/net/codejava/javaee/HelloServlet.java
+++ HelloWorldJavaEE/src/net/codejava/javaee/HelloServlet.java
@@ -33,7 +33,7 @@ public class HelloServlet extends HttpServlet {
 		String age = request.getParameter("age");
 		PrintWriter writer = response.getWriter();
 		writer.println("<h1>Hello " + yourName + "</h1>");
-		writer.println("<h1>Hello your Age is " + age + "</h1>");
+		writer.println("<h1>Hello your Age is : " + age + "</h1>");
 		writer.close();
 	}
 
