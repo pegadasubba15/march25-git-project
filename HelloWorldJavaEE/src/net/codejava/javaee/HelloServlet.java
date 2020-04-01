f1f40c2fdc9d4e378310d61a0b469cbb7dbb5f37
pegadasubba15 <pegadasubba15@gmail.com>
2020-04-01 17:47:53 +0530

    update

--- HelloWorldJavaEE/src/net/codejava/javaee/HelloServlet.java
+++ HelloWorldJavaEE/src/net/codejava/javaee/HelloServlet.java
@@ -33,7 +33,7 @@ public class HelloServlet extends HttpServlet {
 		String age = request.getParameter("age");
 		PrintWriter writer = response.getWriter();
 		writer.println("<h1>Hello " + yourName + "</h1>");
-		writer.println("<h1>Hello " + age + "</h1>");
+		writer.println("<h1>Hello ageeee" + age + "</h1>");
 		writer.close();
 	}
 
